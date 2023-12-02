19a83a4c504523741a58d740d5c271313aa16123
AdityaP1502 <i.m.aditya015@gmail.com>
2023-12-02 16:47:03 +0700

    auth: create genereate refresh token function and change env name for key

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,10 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 function generateAccessToken(userPayload) {
-    return jwt.sign(userPayload, process.env.TOKEN_SECRET_KEY, {
+    return jwt.sign(userPayload, process.env.ACCESS_TOKEN_SECRET_KEY, {
       algorithm: "HS256",  
       expiresIn: "5m",
     });
 }
 
-module.exports = generateAccessToken;
+function generateRefreshToken(userPayload) {
+    return jwt.sign(userPayload, process.env.REFRESH_TOKEN_SECRET_KEY, {
+        algorithm: "HS256",  
+        expiresIn: "24h",
+      });
+}
+
+module.exports = { generateAccessToken, generateRefreshToken };
