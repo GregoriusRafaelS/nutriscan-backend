e649d12a5b82f3f8638e81cd9148549c10769daa
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2023-12-02 23:33:12 +0700

    fix: removed comment in middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,26 +12,18 @@ function authenticationToken(req, res, next) {
     next(error)
   };
 
-    jwt.verify(token, process.env.TOKEN_SECRET_KEY, { algorithms: ['HS256'] }, function(err, decoded) {
-        if (err) {
-          const error = new Error(err.message);
-          error.statusCode = 401;
-          return next(error)
-        }
-
-        const user = {
-          id: decoded.id,
-        };
-
-        req.user = user;
-        next();
-    });
-
-    // if (!decodedToken) {
-    //   const error = new Error("Token is");
-    //   error.statusCode = 401;
-    //   next(error)
-    // };
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, { algorithms: ['HS256'] }, function(err, decoded) {
+    if (err) {
+      const error = new Error(err.message);
+      error.statusCode = 401;
+      return next(error)
+    }
+    const user = {
+      id: decoded.id,
+    };
+    req.user = user;
+    next();
+  });
 }
 
 module.exports = authenticationToken;
