35139767ae8739e21f361bcbe385149bf2864b94
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2023-12-02 15:19:12 +0700

    fix: handle authentication errors gracefully for expired user tokens

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,38 @@
 const jwt = require("jsonwebtoken");
 
 function authenticationToken(req, res, next) {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) next(new Error("Invalid Authentication"));
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) next(new Error("Invalid Authentication"));
 
-    let token;
-    if (authHeader && authHeader.startsWith("Bearer")) token = authHeader.split(" ")[1];
-    if (!token) {
-      const error = new Error("Token is required");
-      error.statusCode = 401;
-      next(error)
-    };
+  let token;
+  if (authHeader && authHeader.startsWith("Bearer")) token = authHeader.split(" ")[1];
+  if (!token) {
+    const error = new Error("Token is required");
+    error.statusCode = 401;
+    next(error)
+  };
 
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY, { algorithms: ['HS256'] });
+  jwt.verify(token, process.env.TOKEN_SECRET_KEY, { algorithms: ['HS256'] },
+  (error, decoded) => {
+    if (error) {
+      let customError;
+      if (error.name === 'TokenExpiredError') {
+        customError = new Error("Token is expired");
+        customError.statusCode = 401;
+      } else {
+        customError = new Error("Invalid Token");
+        customError.statusCode = 401;
+      }
+      return next(customError);
+    }
 
-    if (!decoded) {
-      const error = new Error("Token is");
-      error.statusCode = 401;
-      next(error)
-    };
-
-    const user = {
+    req.user = {
       id: decoded.id,
     };
-    req.user = user;
-
     next();
+  }
+);
+
 }
 
 module.exports = authenticationToken;
