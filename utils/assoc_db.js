b81b79a993555362a0e3988e49d96150781e4bd3
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2023-12-05 03:00:36 +0700

    feat: add an assoc for db and implemented it in model/index.js

--- utils/assoc_db.js
+++ utils/assoc_db.js
@@ -1,12 +1,31 @@
-const { User, Authentications } = sequelize.models;
 
 const associationDB = (sequelize) => {
+  const { User, Authentications, FoodHistory, Food } = sequelize.models;
+
   User.hasMany(Authentications, {
     foreignKey: "id_user",
     sourceKey: 'id',
   });
   Authentications.belongsTo(User, {
     foreignKey: "id_user",
+    targetKey: 'id'
+  });
+  
+  User.hasMany(FoodHistory, {
+    foreignKey: "id_user",
+    sourceKey: 'id',
+  });
+  FoodHistory.belongsTo(User, {
+    foreignKey: "id_user",
+    targetKey: 'id'
+  });
+  
+  Food.hasMany(FoodHistory, {
+    foreignKey: "id_food",
+    sourceKey: 'id',
+  });
+  FoodHistory.belongsTo(Food, {
+    foreignKey: "id_food",
     targetKey: 'id'
   });
 }
