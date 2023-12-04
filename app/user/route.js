eb24190aa47fbb38a2e0cd9e9c50498b32c61dc2
AdityaP1502 <i.m.aditya015@gmail.com>
2023-12-05 05:38:16 +0700

    user: add upload handler middleware in update user

--- app/user/route.js
+++ app/user/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { handlerRegisterUser, handlerLoginUser, handlerUpdateUserProfile } = require("./handler");
 const auth = require("../../middleware/auth");
+const multer = require('multer');
+const multerGoogleStorage = require('multer-google-storage');
 
 const router = express.Router();
 
@@ -10,8 +12,20 @@ router.post("/register", handlerRegisterUser);
 // API Login user: GET users/login
 router.get("/login", handlerLoginUser);
 
-// API UPDATE user: PUUT users/update/:id
-router.put("/update", auth ,handlerUpdateUserProfile);
+// API UPDATE user: PUT users/update/:id
+const uploadHanlder = multer({
+    storage: multerGoogleStorage.storageEngine({
+        bucket: process.env.GCS_BUCKET_NAME, 
+        keyFilename: process.env.GCS_KEYFILE,
+        projectId: process.env.GCS_PROJECT,  
+    }),
 
+    filename: (req, file, cb) => {
+        const ext = file.originalname.split('.').pop();
+        cb(null, `${req.user.id}-avatar.${ext}`);
+    }
+})
+
+router.put("/update", auth, handlerUpdateUserProfile, uploadHanlder);
 
 module.exports = router;
