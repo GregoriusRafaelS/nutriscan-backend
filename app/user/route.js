5a4fdeeb32b33c8518a72ba2a75d81b6faf443f1
AdityaP1502 <i.m.aditya015@gmail.com>
2023-12-05 14:10:54 +0700

    fix: remove previous update route

--- app/user/route.js
+++ app/user/route.js
@@ -12,23 +12,21 @@ router.post("/register", handlerRegisterUser);
 // API Login user: GET users/login
 router.get("/login", handlerLoginUser);
 
-// API UPDATE user: PUT users/update/:id
 const uploadHanlder = multer({
     storage: multerGoogleStorage.storageEngine({
         bucket: process.env.GCS_BUCKET_NAME, 
         keyFilename: process.env.GCS_KEYFILE,
         projectId: process.env.GCS_PROJECT,  
     }),
-
+    
     filename: (req, file, cb) => {
         const ext = file.originalname.split('.').pop();
         cb(null, `${req.user.id}-avatar.${ext}`);
     }
 })
 
-router.put("/update", auth, handlerUpdateUserProfile, uploadHanlder.single('file'));
 // API UPDATE user: PUT users/update/:id
-router.put("/update", auth ,handlerUpdateUserProfile);
+router.put("/update", auth, handlerUpdateUserProfile, uploadHanlder.single('file'));
 
 // API REFRESH TOKEN: POST users/refreshToken
 router.post("/refreshToken", handlerTokenRefresh);
