67f0e750bbf233ffbc12398dc5e50c39e4c4c92b
AdityaP1502 <i.m.aditya015@gmail.com>
2023-12-05 16:40:55 +0700

    feat: add new upload route for avatar

--- app/user/route.js
+++ app/user/route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { handlerRegisterUser, handlerLoginUser, handlerUpdateUserProfile, handlerTokenRefresh } = require("./handler");
+const { handlerRegisterUser, handlerLoginUser, handlerUpdateUserProfile, handlerTokenRefresh, h, handleUploadAvatar } = require("./handler");
 const auth = require("../../middleware/auth");
 const multer = require('multer');
 const multerGoogleStorage = require('multer-google-storage');
+const { validateUpdateProfilePictureUserSchema } = require('../../validator/user');
 
 const router = express.Router();
 
@@ -16,17 +17,38 @@ const uploadHanlder = multer({
     storage: multerGoogleStorage.storageEngine({
         bucket: process.env.GCS_BUCKET_NAME, 
         keyFilename: process.env.GCS_KEYFILE,
-        projectId: process.env.GCS_PROJECT,  
+        projectId: process.env.GCS_PROJECT, 
+        filename: (req, file, cb) => {
+            const ext = file.originalname.split('.').pop();
+            cb(null, `${req.body.username}-avatar.${ext}`);
+        },
     }),
     
-    filename: (req, file, cb) => {
-        const ext = file.originalname.split('.').pop();
-        cb(null, `${req.user.id}-avatar.${ext}`);
+    fileFilter: (req, file, cb) => {
+        try {
+            validateUpdateProfilePictureUserSchema({
+                fieldname: file.fieldname,
+                mimetype: file.mimetype,
+                filename:file.originalname
+            })
+        } catch (err) {
+            req.fileValidationError = err.message
+            return cb(null, false, new Error(err.message))
+        }
+
+        if (req.body.username === undefined |! req.body.username === null) {
+            req.fileValidationError = "Username field is empty"
+            return cb(null, false, new Error("Username filed is empty"))
+        }
+        
+        cb(null, true)
     }
 })
 
 // API UPDATE user: PUT users/update/:id
-router.put("/update", auth, handlerUpdateUserProfile, uploadHanlder.single('file'));
+router.put("/update", auth, handlerUpdateUserProfile);
+
+router.post('/avatar/upload', auth, uploadHanlder.single('avatar'), handleUploadAvatar)
 
 // API REFRESH TOKEN: POST users/refreshToken
 router.post("/refreshToken", handlerTokenRefresh);
