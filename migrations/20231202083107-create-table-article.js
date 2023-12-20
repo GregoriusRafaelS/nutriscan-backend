b998b89f076d70b080bb5af3d588b8ad85c69c3f
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2023-12-20 21:03:09 +0700

    fix: minor typo

--- migrations/20231202083107-create-table-article.js
+++ migrations/20231202083107-create-table-article.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Articles', {
+    await queryInterface.createTable('articles', {
       id: {
         allowNull: false,
         autoIncrement: true,
